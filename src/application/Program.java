aeb6375767b3d44f9a19637354acbe687f96b8b0
proleonardopa@gmail.com <60435208+rookie-leo@users.noreply.github.com>
2021-01-05 10:07:45 -0300

    solution with oop

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,29 +3,31 @@ package application;
 import java.util.Locale;
 import java.util.Scanner;
 
+import entities.Triangle;
+
 public class Program {
 
 	public static void main(String[] args) {
 		
 		Locale.setDefault(Locale.US);
 		Scanner kb = new Scanner(System.in);
-		double xA, xB, xC, yA, yB, yC;
+		Triangle x = new Triangle();
+		Triangle y = new Triangle();
 		
 		System.out.println("Enter the measures of triangle X: ");
-		xA = kb.nextDouble();
-		xB = kb.nextDouble();
-		xC = kb.nextDouble();
+		x.setA(kb.nextDouble());
+		x.setB(kb.nextDouble()); 
+		x.setC(kb.nextDouble());
 		
 		System.out.println("Enter the measures of triangle Y: ");
-		yA = kb.nextDouble();
-		yB = kb.nextDouble();
-		yC = kb.nextDouble();
+		y.setA(kb.nextDouble());
+		y.setB(kb.nextDouble()); 
+		y.setC(kb.nextDouble());
 		
-		double p = (xA + xB + xC) / 2.0;
-		double areaX = Math.sqrt(p * (p - xA) * (p - xB) * (p - xC));
-
-		p = (yA + yB + yC) / 2.0;
-		double areaY = Math.sqrt(p * (p - yA) * (p - yB) * (p - yC));
+		double areaX = x.area(x.getA(), x.getB(), x.getC());
+		
+			
+		double areaY = y.area(y.getA(), y.getB(), y.getC());
 		
 		System.out.printf("Triangle X area: %.4f%n", areaX);
 		System.out.printf("Tirangle Y area: %.4f\n", areaY);
--- src/entities/Triangle.java
+++ src/entities/Triangle.java
@@ -2,5 +2,49 @@ package entities;
 
 public class Triangle {
 
+	private double a, b, c;
+	
+	public Triangle() {}
+	
+	
+	
+	public Triangle(double a, double b, double c) {
+		this.a = a;
+		this.b = b;
+		this.c = c;
+	}
+
+
+
+	public double area(double a, double b, double c) {
+		double p = (a + b + c) / 2.0;
+		double area = Math.sqrt(p * (p - a) * (p - b) * (p - c));
+		return area;
+	}
+
+	public double getA() {
+		return a;
+	}
+
+	public void setA(double a) {
+		this.a = a;
+	}
+
+	public double getB() {
+		return b;
+	}
+
+	public void setB(double b) {
+		this.b = b;
+	}
+
+	public double getC() {
+		return c;
+	}
+
+	public void setC(double c) {
+		this.c = c;
+	}
+	
 	
 }
